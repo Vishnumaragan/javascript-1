bb9d812d3800d8c1722f037f8ad25995c4257481
nairx <nairxprv@gmail.com>
2026-02-11 14:36:57 +0530

    updated

--- part1/file6.js
+++ part1/file6.js
@@ -8,9 +8,14 @@
 //     }
 // }
 
-for(let i=1;i<=5;i++){
-    if (i==3){
-        continue
-    }
+// for(let i=1;i<=5;i++){
+//     if (i==3){
+//         continue
+//     }
+//     console.log(i)
+// }
+
+//Using for loop print 5,10,15...30
+for (let i=5;i<=30;i+=5){
     console.log(i)
 }
