ec2b9c8a17ff2871028c835a3641b6e74f33a6b6
nairx <nairxprv@gmail.com>
2026-02-13 15:07:40 +0530

    updated

--- part3/file27.js
+++ part3/file27.js
@@ -13,9 +13,42 @@
 // console.log(result)
 
 // let score = 9
-// let result = score > 50 && "Pass" 
+// let result = score > 50 && "Pass"
 // console.log(result)
 
-let score
-let result = score || 30 
-console.log(result)
+// let score
+// let result = score || 30
+// console.log(result)
+
+// let score = 70;
+// switch (true){
+//     case (score<=50):{
+//         console.log("Grade C");
+//         break;
+//     }
+//     case (score<=80):{
+//         console.log("Grade B");
+//         break;
+//     }
+//     case (score<=100):{
+//         console.log("Grade A");
+//         break;
+//     }
+//     default:
+//         console.log("Failed");
+// }
+
+let subject = "1";
+switch (subject) {
+  case "1": {
+    console.log("HyperText Markup Lanugage");
+    break;
+  }
+  case "2": {
+    console.log("Cascading Style Sheet");
+    break;
+  }
+  default: {
+    console.log("Unknown");
+  }
+}
