9abc735490014003ca8655e8cfa577f994a1080b
nairx <nairxprv@gmail.com>
2026-02-17 10:14:50 +0530

    updated

--- part5/file43.js
+++ part5/file43.js
@@ -9,7 +9,8 @@ function f1() {
 function f2() {
   console.log("This is f2 function");
 }
-f1()
-  .then(() => f2())
-  .catch((err) => console.log(err));
-
+async function main() {
+  await f1();
+  f2();
+}
+main();
---
+++ part5/file45.js
@@ -0,0 +1,21 @@
+function getStudentInfo() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const userObj = {
+        name: "John",
+        email: "john@gmail.com",
+        age: 21,
+      };
+      resolve(userObj);
+    }, 2000);
+  });
+}
+async function main() {
+  const result = await getStudentInfo();
+  console.log(result);
+  console.log("Program Completed Successfully");
+}
+
+main()
+
+//getStudentInfo should return object name,email and age
